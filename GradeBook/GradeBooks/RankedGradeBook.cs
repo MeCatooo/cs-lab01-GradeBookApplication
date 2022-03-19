1bcaf2580a24a8bdaf27adc1ba322656a4a4aa8e
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-03-19 11:35:40 +0100

    10/24 GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -8,6 +9,35 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            int studentsPerTwentyPercent = (int)(Students.Count * 0.2);
+            int rank = 1;
+            foreach (var student in Students)
+            {
+                double studentAverage = student.Grades.Sum() / student.Grades.Count;
+                if (studentAverage > averageGrade)
+                {
+                    rank++;
+                }
+            }
+            int ranking = (int)Math.Ceiling((double)(rank / studentsPerTwentyPercent));
+            switch (ranking)
+            {
+                case 1:
+                    return 'A';
+                case 2:
+                    return 'B';
+                case 3:
+                    return 'C';
+                case 4:
+                    return 'D';
+                default:
+                    return 'F';
+            }
         }
     }
 }
